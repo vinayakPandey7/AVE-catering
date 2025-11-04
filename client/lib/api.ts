e3616ebb1fb0d80654b727a63c653f6a3261a369
Amit <amitnehraa@gmail.com>
2025-11-04 20:56:12 +0530

    Add api end point

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://ave-catering.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
