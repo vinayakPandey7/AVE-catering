a1a9705d2cba639e40cc9e3d2ff4c4b8c99ea982
Amit <amitnehraa@gmail.com>
2025-11-08 16:20:00 +0530

    Update API configuration

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  "https://localhost:5000/api",
+  baseURL:  "https://ave-catering.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
